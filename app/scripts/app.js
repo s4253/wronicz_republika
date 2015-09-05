7738eafdfa900ac5e65f5c73e1685c1454362533
jacek <jacek.malolepszy@itcrowd.pl>
2015-09-05 03:41:20 +0200

    Copy basic content

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,6 +23,47 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/world', {
+        templateUrl: 'views/world.html',
+        controller: 'AboutCtrl'
+      }).when('/poland', {
+        templateUrl: 'views/poland.html',
+        controller: 'AboutCtrl'
+      }).when('/maximum', {
+        templateUrl: 'views/maximum.html',
+        controller: 'AboutCtrl'
+      }).when('/groups', {
+        templateUrl: 'views/groups.html',
+        controller: 'AboutCtrl'
+      })
+
+
+      .when('/grandmasters', {
+        templateUrl: 'views/grandmasters.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/tournaments', {
+        templateUrl: 'views/tournaments.html',
+        controller: 'AboutCtrl'
+      }).when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/rulebook', {
+        templateUrl: 'views/rulebook.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/rules', {
+        templateUrl: 'views/rules.html',
+        controller: 'AboutCtrl'
+      }).when('/explanations', {
+        templateUrl: 'views/explanations.html',
+        controller: 'AboutCtrl'
+      }).when('/gamesdb', {
+        templateUrl: 'views/gamesdb.html',
+        controller: 'AboutCtrl'
+      })
+
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
